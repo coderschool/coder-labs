0e38e78432f55ea1888730a4b8956de78be16ff1
Luke Hoang <lukehoang195@gmail.com>
2022-09-22 17:46:41 +0700

    move Course/index.js to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,128 @@
-import Link from "next/link";
-import React from "react";
+import { useEffect, useState, useCallback } from "react";
+import { useRouter } from "next/router";
 
-const index = () => {
-  return (
-    <>
-      <h1>Welcome to CoderSchool content 2.0 (beta)</h1>
-      <ul>
-        <h4>Demo</h4>
-        <li>
-          <Link href={"demo/coder-labs"}>coder-labs</Link>
-        </li>
-        <li>
-          <Link href={"demo/intro-coder-labs"}>intro-coder-labs</Link>
-        </li>
-        <li>
-          <Link href={"demo/practice"}>practice</Link>
-        </li>
-      </ul>
-      <ul>
-        <h4>Web Development</h4>
-        <li>
-          <Link href={"wv/m13/s2-a1s-calculator"}>Calculator Solution</Link>
-        </li>
-      </ul>
-    </>
+// hooks
+import useIsMountedRef from "../src/hooks/useIsMountedRef";
+// utils
+import axios from "../src/utils/axios";
+// mui
+import { alpha, styled } from "@mui/material/styles";
+import { Typography, Container, Box, Button } from "@mui/material";
+import { TreeView, TreeItem, treeItemClasses } from "@mui/lab";
+
+// components
+import Page from "../src/components/Page";
+import Logo from "../src/components/Logo";
+import { Block } from "../src/components/sections/Block";
+
+const RootStyle = styled("div")(({ theme }) => ({
+  paddingTop: theme.spacing(11),
+  paddingBottom: theme.spacing(15),
+}));
+
+const TreeViewStyle = styled(TreeView)({
+  height: 240,
+  flexGrow: 1,
+  maxWidth: 400,
+});
+
+const StyledTreeItem = styled((props) => <TreeItem {...props} />)(
+  ({ theme }) => ({
+    [`& .${treeItemClasses.iconContainer}`]: {
+      "& .close": {
+        opacity: 0.3,
+      },
+    },
+    [`& .${treeItemClasses.group}`]: {
+      marginLeft: 15,
+      paddingLeft: 18,
+      borderLeft: `1px dashed ${alpha(theme.palette.text.primary, 0.4)}`,
+    },
+  })
+);
+
+export default function CourseDetail() {
+  const isMountedRef = useIsMountedRef();
+  const router = useRouter();
+
+  const [courses, setCourses] = useState([]);
+  const [error, setError] = useState(null);
+
+  const getCourses = useCallback(async () => {
+    try {
+      const response = await axios.get(`/api/courses`);
+
+      if (isMountedRef.current) {
+        setCourses(response.data);
+      }
+    } catch (error) {
+      console.error(error);
+      setError(error.message);
+    }
+  }, [isMountedRef]);
+
+  useEffect(() => {
+    getCourses();
+  }, [getCourses]);
+
+  const handleClickNode = (pathname) => {
+    if (pathname) {
+      router.push(pathname);
+    }
+  };
+
+  const renderTree = (nodes) => (
+    <StyledTreeItem
+      key={nodes.id}
+      nodeId={nodes.id}
+      label={nodes.title}
+      onClick={() => handleClickNode(nodes.path)}
+    >
+      {Array.isArray(nodes.courseUnits)
+        ? nodes.courseUnits.map((node) => renderTree(node))
+        : null}
+    </StyledTreeItem>
   );
-};
 
-export default index;
+  return (
+    <Page title="Courses">
+      <Container>
+        {error && <Typography variant="h6">404 {error}!</Typography>}
+
+        <Typography variant="h2" sx={{ display: "flex", alignItems: "center" }}>
+          <Logo disabledLink sx={{ display: "inline-block", mr: 2 }} /> Welcome
+          to CoderSchool
+        </Typography>
+
+        <RootStyle>
+          <Box
+            sx={{
+              display: "grid",
+              gap: 3,
+              gridTemplateColumns: {
+                xs: "repeat(1, 1fr)",
+                md: "repeat(3, 1fr)",
+              },
+            }}
+          >
+            {courses.length ? (
+              courses.map((course) => (
+                <Block key={course.id} title={course.title}>
+                  <TreeViewStyle defaultExpanded={[course.id]}>
+                    {course.courseUnits?.length ? (
+                      renderTree(course)
+                    ) : (
+                      <Typography variant="subtitle2">No Lab found.</Typography>
+                    )}
+                  </TreeViewStyle>
+                </Block>
+              ))
+            ) : (
+              <Typography variant="h6">No course found!</Typography>
+            )}
+          </Box>
+        </RootStyle>
+      </Container>
+    </Page>
+  );
+}
