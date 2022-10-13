a74ae04ebac142efe943850d81cee20d90c8c990
Minh Do <minhdh@coderschool.vn>
2022-10-13 15:19:10 +0700

    CoderPrep: 10 ideas for web/js

---
+++ src/components/TextIconLabel.js
@@ -0,0 +1,28 @@
+// @mui
+import { Stack } from "@mui/material";
+
+// ----------------------------------------------------------------------
+
+export default function TextIconLabel({
+  icon,
+  value,
+  endIcon = false,
+  sx,
+  ...other
+}) {
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      sx={{
+        typography: "body2",
+        ...sx,
+      }}
+      {...other}
+    >
+      {!endIcon && icon}
+      {value}
+      {endIcon && icon}
+    </Stack>
+  );
+}
