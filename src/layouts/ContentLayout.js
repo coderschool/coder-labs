5651e87382d136305389a39db1ddcca28ccf530f
Luke Hoang <lukehoang195@gmail.com>
2022-10-17 11:22:04 +0700

    add layout for content, select all list tag

---
+++ src/layouts/ContentLayout.js
@@ -0,0 +1,9 @@
+import { Container } from "@mui/material";
+
+const ContentLayout = ({ children }) => (
+  <Container>
+    <main>{children}</main>
+  </Container>
+);
+
+export default ContentLayout;
